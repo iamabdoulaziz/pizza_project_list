9610b15b892b0483f9d89a414e947391788c8622
Cute Boy <abdoulazizc867@gmail.com>
2024-12-05 14:52:43 +0000

    Exercise : Allow user to add a new pizza at the end of the tuple and print the new tuple, this is my version.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 
 pizzas = ("4 fromages", "Véhétarienne", "Hawai", "Calzone")
 
-def print_pizza():
-    print(f"---- LISTE DES PIZZAS ({len(pizzas)})----")
-    for pizza in pizzas:
-        print(pizza)
+def print_pizza(collection):
+    print(f"---- LISTE DES PIZZAS ({len(collection)})----")
+    if not collection:
+        print("AUCUNE PIZZA")
+    else:
+        for pizza in collection:
+            print(pizza)
+        print()
+        print(f"# La prémière pizza : {collection[0]}")
+        print(f"# La dernière pizza : {collection[-1]}")
 
 
-print_pizza()
+def user_add_pizza(collection):
+    user = input("Pizza à ajouter : ")
+    add_pizza = user
+    collection = collection + (add_pizza,)
+    return collection
+
+
+empty_pizza_list = ()
+
+pizzas = user_add_pizza(pizzas)
+
+print_pizza(pizzas)
